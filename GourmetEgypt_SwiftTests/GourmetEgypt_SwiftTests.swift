4b64a13375c835680c706b34e4d57f7f3a04bbbd
ashutosh-bravvura <amishra@bravvura.in>
2017-04-25 19:58:21 +0530

    Initial Swift setup
    It is initial swift setup

---
+++ GourmetEgypt_SwiftTests/GourmetEgypt_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  GourmetEgypt_SwiftTests.swift
+//  GourmetEgypt_SwiftTests
+//
+//  Created by Ashutosh Mishra on 25/04/17.
+//  Copyright © 2017 Ashutosh Mishra. All rights reserved.
+//
+
+import XCTest
+@testable import GourmetEgypt_Swift
+
+class GourmetEgypt_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
